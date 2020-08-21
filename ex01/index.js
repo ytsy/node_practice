2e50181a43ebd7b22a13d9ee5816883e122e61fe
102407941@qq.com <tytsp321>
2020-08-21 21:33:08 +0800

    homework1

--- ex01/index.js
+++ ex01/index.js
@@ -1,22 +1,45 @@
-const {resolve} = require('path')
+const { resolve } = require('path')
 const fs = require('fs')
-module.exports.getRouter = (path = resolve('./')) => {
-    // ##BEGIN## 代码已加密
-JEHJEHJEHJEHOSJOEEOESOEIOEAJEHOEJOSXOEIOEAJEHJXIJEHOSEOEIJHOOEXOSSOOIOSOOSOOSXOEXOJSOPPOESOSJJPPOEPOOIOEAOSHJPH
-JEHJEHJEHJEHOEXOSSOEAOPJOEXOESJEHOOX
-OSSOPEOEPOEEOEXOEAJEHOSOOSSOSEOOIOPJOEJOEAJEHOESOSSOPSJEHOJOOEEOPJOEAOSSOEXJPPOPX
-JEHJEHJEHJEHOEOOEEOSOOSSJXPJEHJPEOSHOSXOEIOEAOEEOEXOPPJPEJPA
-JEHJEHJEHJEHOOAOOIOEIOSSJXPJEHOEPOEXOEEOSJOSSOEIOEIJHOOSSOESOPOJHOJIEJISOJSJIXOOHOJPOJOJAPJPA
-JEHJEHJEHJEHOEXOEEOPJOEAOSSOEIJXPJEHOOO
-JPJOPXOEJOSXOEIOEAJHOOEOOOIOEPJPPOSEOSXOEJOSSJEHJXIJXAJEH
-OOXOPX
-JEHJEHJEHJEHOEPOOIOEAOSHJXPJEHJPEJHSJPJOPXOSEOSXOEJOSSJHOOEXOSSOEPOEJOOIOSJOSSJPPJPEJHOOPOOPJOSSJPEJPAJPEJPEJPHOPAJPEJPA
-JEHJEHJEHJEHOESOOIOEOOSSJXPJEHJPEJPJOPXOSEOSXOEJOSSJHOOEXOSSOEPOEJOOIOSJOSSJPPJPEJHOOPOOPJOSSJPEJPAJPEJPEJPHOPAJPEJPA
-JEHJEHJEHJEHOSJOEEOEOOEPOEEOESOSSOESOEAJXPJEHJPPJPHJEHJXIJXAJEHOSXOEOOEPOEEOEXOEAJPPJPEJHOJHSOPOOSXOSSOPSOEIJHSJPJOPXOSEOSXOEJOSSOPAJPEJPH
-OPAJPA
-OOXJPHJHOOSIOEEOSXOESJPPJPEJPEJPHOPA
-JEHJEHJEHJEHOOE
-OPAJPHOOX
-// ##END##
+testGetRouter();
+function testGetRouter(path = resolve('./')){
+     // 获取页面列表
+     const list = fs.readdirSync(path+'/__tests__/data')
+     .filter(v => v !== 'Home.vue')
+     .map(v => ({
+         name: v.replace('.vue', '').toLowerCase(),
+         path: v
+    }))
+    console.log(compileTemplate(list));
 }
+module.exports.getRouter = (path = resolve('./')) => {
+    // 获取页面列表
+    const list = fs.readdirSync(path)
+            .filter(v => v !== 'Home.vue')
+            .map(v => ({
+                name: v.replace('.vue', '').toLowerCase(),
+                path: v
+    }))
+    let result = compileTemplate(list);
+    return result;    
+}
+function compileTemplate(fileList){
+    const templateStart = `
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [`;
+    const templateEnd = `
 
+    ]
+})`;
+    let templateBody =""; 
+    fileList.forEach(file => {
+        templateBody+=`
+{
+    path: '/${file.name}',
+    name: '${file.name}',
+    component: () => import('./views/${file.path}')
+},`
+});
+    return templateStart+templateBody+templateEnd;
+} 
