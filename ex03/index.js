9634f4a74824929a2a3689448c002d44967dcca8
102407941@qq.com <tytsp321>
2020-08-25 00:26:11 +0800

    h3

--- ex03/index.js
+++ ex03/index.js
@@ -1,18 +1,32 @@
-const fs = require('fs')
+const fs = require('fs');
+// function parser (path){
+//     const readStream = fs.createReadStream(path)
+//     let reqData = [];
+//     let size = 0;
+//     return new Promise(resolve => {
+//         readStream.on("data",(chunk)=>{
+//             reqData.push(chunk);
+//             size+=chunk.length;
+//         })
+//         readStream.on("end",()=>{
+//             let JsonString = Buffer.concat(reqData,size).toString('utf8')
+//             resolve(JSON.parse(JsonString))
+//         })
+//     })
+// }
+//暗号：二分查找
 module.exports.parser = path => {
     const readStream = fs.createReadStream(path)
     let reqData = [];
     let size = 0;
     return new Promise(resolve => {
-         // ##BEGIN## 代码已加密
-JEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOOIOSOOJSOEAOEXOSSOOIOEOJHOOEEOESJPPJPEOSOOOIOEAOOIJPEJPAJEHOSOOOIOEAOOIJEHJXIJXAJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEHJIHOOIOEAOOIJHOOEPOPJOEIOSHJPPOSOOOIOEAOOIJPHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEIOSXOPHOSSJEHJPIJXIJEHOSOOOIOEAOOIJHOOEJOSSOESOSPOEAOSHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHOPAJPHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOOIOSOOJSOEAOEXOSSOOIOEOJHOOEEOESJPPJPEOSSOESOSOJPEJPAJEHOSEOPJOESOSJOEAOSXOEEOESJEHJPPJPHJEHOPX
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOSJOEEOESOEIOEAJEHOSOOOIOEAOOIJEHJXIJEHJIEOPJOSEOSEOSSOEXJHOOSJOEEOESOSJOOIOEAJPPOEXOSSOEHJIHOOIOEAOOIJPAJEHOEIOSXOPHOSSJPHJXH
-JEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHJEHOEXOSSOEIOEEOEJOPOOSSJPPJASOJSJAIJAXJHOOEPOOIOEXOEIOSSJPPOSOOOIOEAOOIJHOOEAOEEOJSOEAOEXOSXOESOSPJPPJPHJPHJPH
-JEHJEHJEHJEHJEHJEHJEHJEHOPAJPHJXH
-         // ##END##
+        readStream.on("data",(chunk)=>{
+            reqData.push(chunk);
+            size+=chunk.length;
+        })
+        readStream.on("end",()=>{
+            let JsonString = Buffer.concat(reqData,size).toString('utf8')
+            resolve(JSON.parse(JsonString))
+        })
     })
 }
